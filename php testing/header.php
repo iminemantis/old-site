f827aeddfafc0d345c42f6dc3898a9b861d22bcc
iminemantis <90356983+iminemantis@users.noreply.github.com>
2021-09-09 21:38:19 +0100

    fue changes tried php
    changed a fue things around and started  messing with php but couldnt get anything to work

---
+++ php testing/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>hello</title>
+	<link rel="stylesheet" href="../stylesheet.css">
+	
+</head>
+<body>
+	<div class="header" id="myHeader">
+		<a onclick="openNav()">open</a>
+		<a href="index.html"><img src="" alt="page1"</a>
+		<a href="page2.html"><img src="" alt="page2"></a>
+	</div>
+
+	<script src="../code.js"></script>
+</body>
+</html>
---
+++ php testing/page.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<head>
+	<title>page</title>
+	<link rel="stylesheet" href="../stylsheet.css">
+</head>
+<body>
+	<?php include_once "header.php"; ?>
+	
+</body>
+</html>
